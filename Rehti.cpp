413268acaac58698f2ef8641369704405f7e9cc3
OsoWiz <otso.laasonen@gmail.com>
2024-05-25 15:43:19 +0300

    Add project files.

---
+++ Rehti.cpp
@@ -0,0 +1,11 @@
+﻿// Rehti.cpp : Defines the entry point for the application.
+//
+
+#include "Rehti.h"
+
+using namespace std;
+
+void initialize()
+{
+	cout << "Hello CMake." << endl;
+}
---
+++ Rehti.h
@@ -0,0 +1,7 @@
+﻿// Rehti.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
